30b48cb2cf69e5280ee5c768e6c5c80d9d1bae89
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-07-22 11:13:58 +0200

    UI: use FLAG_LAYOUT_NO_LIMITS for transparent bars

--- app/src/main/java/com/feder/compose/MainActivity.kt
+++ app/src/main/java/com/feder/compose/MainActivity.kt
@@ -131,8 +131,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         try {
-            window.statusBarColor = 0x00000000  // прозрачный
-            window.navigationBarColor = 0x00000000  // прозрачный
+            // Прозрачные бары для Android 11+
+            window.setFlags(
+                android.view.WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                android.view.WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
             setContent { FederTheme { FederApp() } }
         }
         catch (e: Exception) { Toast.makeText(this, "Ошибка запуска", Toast.LENGTH_LONG).show() }
