442bde16a84bb18492b2abf3cedcaa7eda309f7e
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-07-24 16:05:23 +0200

    fix: use Color instead of SurfaceContainer in Surface

--- app/src/main/java/com/feder/compose/ui/screen/ChatScreen.kt
+++ app/src/main/java/com/feder/compose/ui/screen/ChatScreen.kt
@@ -129,7 +129,7 @@ fun ChatScreen(chatName: String, chatUsername: String, myUsername: String, onBac
         Column(modifier = Modifier.fillMaxSize()) {
             // Selection Bar
             if (selectionMode) {
-                Surface(color = SurfaceContainer, shadowElevation = 4.dp) {
+                Surface(color = Color(0xFF201F1F), shadowElevation = 4.dp) {
                     Row(
                         Modifier.fillMaxWidth().statusBarsPadding().padding(horizontal = 4.dp, vertical = 4.dp),
                         verticalAlignment = Alignment.CenterVertically
