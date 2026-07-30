a7faf896acd7638c25397fa32a9ed922ec9ecff8
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-07-30 21:20:31 +0200

    fix: add itemsIndexed import

--- app/src/main/java/com/feder/compose/ui/screen/ChatScreen.kt
+++ app/src/main/java/com/feder/compose/ui/screen/ChatScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
