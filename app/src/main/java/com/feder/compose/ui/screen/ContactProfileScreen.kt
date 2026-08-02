8823ad4abc8cffa347b7ba121a00092cdbf787d3
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-08-02 14:44:16 +0200

    fix: add missing imports ContactProfile

--- app/src/main/java/com/feder/compose/ui/screen/ContactProfileScreen.kt
+++ app/src/main/java/com/feder/compose/ui/screen/ContactProfileScreen.kt
@@ -18,6 +18,10 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.feder.compose.ui.theme.*
