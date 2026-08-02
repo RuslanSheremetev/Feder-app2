2363c907981eb264c10da30cf8a3e3429f9db120
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-08-02 13:28:43 +0200

    fix: add ContentScale import

--- app/src/main/java/com/feder/compose/ui/screen/ChatScreen.kt
+++ app/src/main/java/com/feder/compose/ui/screen/ChatScreen.kt
@@ -38,6 +38,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.layout.ContentScale
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.feder.compose.WebSocketManager
