8cc08508191ccbe998c9ab868244322f6a189e01
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-08-05 04:04:18 +0200

    Fix: posX/posY as var for reassignment

--- app/src/main/java/com/feder/compose/ui/screen/ChatScreen.kt
+++ app/src/main/java/com/feder/compose/ui/screen/ChatScreen.kt
@@ -67,8 +67,8 @@ data class MsgItem(
     var status: String = "sent",
     val timeVal: Long = 0L,
     val id: Int = 0,
-    val posX: Float = 0f,
-    val posY: Float = 0f,
+    var posX: Float = 0f,
+    var posY: Float = 0f,
 )
 
 @OptIn(ExperimentalFoundationApi::class)
