aa38c590170e28d504cb2575b629c4b467bb94ab
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-08-08 20:30:21 +0200

    Fix: back from profile returns to chat

--- app/src/main/java/com/feder/compose/MainActivity.kt
+++ app/src/main/java/com/feder/compose/MainActivity.kt
@@ -201,7 +201,7 @@ fun FederApp() {
                     phone = "",
                     bio = "",
                     birthday = "",
-                    onBack = { viewModel.selectedProfile = null }
+                    onBack = { viewModel.selectedProfile = null; viewModel.selectedChat = viewModel.selectedProfile }
                 )
             }
         }
