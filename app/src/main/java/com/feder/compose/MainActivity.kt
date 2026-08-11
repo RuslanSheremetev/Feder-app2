15791f271192a117f711bb41532fe1235da7907d
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-08-11 23:40:08 +0200

    Remove enableEdgeToEdge

--- app/src/main/java/com/feder/compose/MainActivity.kt
+++ app/src/main/java/com/feder/compose/MainActivity.kt
@@ -147,7 +147,6 @@ class ChatViewModel : ViewModel() {
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         try {
             // Прозрачные бары для Android 11+
             window.setFlags(
