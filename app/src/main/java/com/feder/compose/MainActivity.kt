fbb2c255e043e110df5a37e13082ca0891b3f744
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-08-13 09:48:18 +0200

    Move MoreVert slightly left

--- app/src/main/java/com/feder/compose/MainActivity.kt
+++ app/src/main/java/com/feder/compose/MainActivity.kt
@@ -274,7 +274,7 @@ fun FederApp() {
                     Icons.Filled.MoreVert,
                     "menu",
                     tint = Color.White,
-                    modifier = Modifier.size(24.dp).clickable { /* меню */ }
+                    modifier = Modifier.padding(start = 4.dp).size(24.dp).clickable { /* меню */ }
                 )
             }
             }
