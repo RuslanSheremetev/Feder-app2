9f2ae0e6f80ad229487c1556278b5cb8bb8de3c1
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-08-13 09:36:55 +0200

    Stories swipe via NestedScroll onPostScroll

--- app/src/main/java/com/feder/compose/MainActivity.kt
+++ app/src/main/java/com/feder/compose/MainActivity.kt
@@ -18,8 +18,10 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.gestures.detectVerticalDragGestures
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
