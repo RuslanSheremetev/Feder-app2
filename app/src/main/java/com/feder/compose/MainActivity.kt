70f3c2988b3d32563e7727c1d32e8f47d7f3d50c
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-08-15 15:07:53 +0200

    Add sorting back to onSend

--- app/src/main/java/com/feder/compose/MainActivity.kt
+++ app/src/main/java/com/feder/compose/MainActivity.kt
@@ -143,6 +143,7 @@ class ChatViewModel : ViewModel() {
                         timestamp = java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss", java.util.Locale.US).format(java.util.Date())
                 } else chat
             }
+            chats = chats.sortedByDescending { it.timestamp }
             }
         }
         ws.onMessage { sender, msgText, timeVal, msgId ->
