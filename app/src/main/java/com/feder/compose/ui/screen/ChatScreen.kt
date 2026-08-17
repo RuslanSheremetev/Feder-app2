6aa505bf127c75028c5af9699afb2f42905c094e
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-08-17 15:31:14 +0200

    Make fullScreenPhoto global

--- app/src/main/java/com/feder/compose/ui/screen/ChatScreen.kt
+++ app/src/main/java/com/feder/compose/ui/screen/ChatScreen.kt
@@ -86,6 +86,8 @@ import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
 
+var fullScreenPhoto: String? = null
+
 data class MsgItem(
     val from: String,
     val to: String,
@@ -295,7 +297,6 @@ fun ChatScreen(chatName: String, chatUsername: String, myUsername: String, token
     var emojiExpanded by remember { mutableStateOf(false) }
     var attachExpanded by remember { mutableStateOf(false) }
     var selectedPhotos by remember { mutableStateOf<Set<android.net.Uri>>(emptySet()) }
-    var fullScreenPhoto by remember { mutableStateOf<String?>(null) }
     val photoPicker = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.PickMultipleVisualMedia(maxItems = 10)
     ) { uris ->
