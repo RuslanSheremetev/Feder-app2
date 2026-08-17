65b23335ed289dc31d2f1946bdf319d4f203bdc6
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-08-17 11:11:50 +0200

    Fix: close if block in MessageBubble

--- app/src/main/java/com/feder/compose/ui/screen/ChatScreen.kt
+++ app/src/main/java/com/feder/compose/ui/screen/ChatScreen.kt
@@ -254,6 +254,7 @@ fun MessageBubble(msg: MsgItem, text: String, time: String, isMine: Boolean, pos
                         }
                     }
                 }
+                }
             }
         }
 }
