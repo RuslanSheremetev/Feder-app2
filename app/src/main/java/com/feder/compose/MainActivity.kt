d7ece227efef80eb910c117e59291d080d103ca6
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-08-18 14:34:51 +0200

    Fix: Create WebSocketManager only if null

--- app/src/main/java/com/feder/compose/MainActivity.kt
+++ app/src/main/java/com/feder/compose/MainActivity.kt
@@ -166,7 +166,9 @@ class ChatViewModel : ViewModel() {
                 override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) { response.close() }
             })
         } catch (_: Exception) {}
-        wsManager = WebSocketManager(serverUrl = "2.26.71.102", port = 8002)
+        if (wsManager == null) {
+            wsManager = WebSocketManager(serverUrl = "2.26.71.102", port = 8002)
+        }
         val ws = wsManager!!
         ws.onStatus { status ->
             wsStatus = status
