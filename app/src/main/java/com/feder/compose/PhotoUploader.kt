b03332b6884a9658046ec38206b3577f51a8a42e
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-08-20 01:57:40 +0200

    Increase OkHttp timeouts to 120s

--- app/src/main/java/com/feder/compose/PhotoUploader.kt
+++ app/src/main/java/com/feder/compose/PhotoUploader.kt
@@ -11,9 +11,9 @@ import java.util.concurrent.TimeUnit
 
 object PhotoUploader {
     private val client = OkHttpClient.Builder()
-        .connectTimeout(60, TimeUnit.SECONDS)
-        .writeTimeout(60, TimeUnit.SECONDS)
-        .readTimeout(60, TimeUnit.SECONDS)
+        .connectTimeout(120, TimeUnit.SECONDS)
+        .writeTimeout(120, TimeUnit.SECONDS)
+        .readTimeout(120, TimeUnit.SECONDS)
         .retryOnConnectionFailure(true)
         .build()
 
