d54892700fb93b9260e52fb5714afbbc67977148
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-08-20 02:21:16 +0200

    Add Connection: close to upload request

--- app/src/main/java/com/feder/compose/PhotoUploader.kt
+++ app/src/main/java/com/feder/compose/PhotoUploader.kt
@@ -74,6 +74,7 @@ object PhotoUploader {
             val request = Request.Builder()
                 .url("http://2.26.71.102:8002/api/upload")
                 .header("Authorization", "Bearer $token")
+                .header("Connection", "close")
                 .post(body)
                 .build()
 
