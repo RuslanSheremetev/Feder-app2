f01e20b0f980b04d17d75812a3de0bd8a38f162d
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-09-03 11:37:34 +0200

    Remove auto-send from photoPicker

--- app/src/main/java/com/feder/compose/ui/screen/ChatScreen.kt
+++ app/src/main/java/com/feder/compose/ui/screen/ChatScreen.kt
@@ -356,8 +356,7 @@ fun ChatScreen(chatName: String, chatUsername: String, myUsername: String, token
             selectedPhotos = setOf(uri)
             showAttachSheet = false
             attachExpanded = false
-            // Автоматически отправляем фото
-            sendMessage()
+
         }
     }
     var forwardSearch by remember { mutableStateOf("") }
